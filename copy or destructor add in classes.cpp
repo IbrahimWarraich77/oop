c08005528527b3a8fd0c4680b73ae1d585ecd37c
IbrahimWarraich77 <126850734+IbrahimWarraich77@users.noreply.github.com>
2023-03-07 11:32:30 -0800

    Add files via upload

---
+++ copy or destructor add in classes.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	public:
+	int sapid;
+	int semester;
+	string name;
+	float cgpa;
+	student()
+	{
+		sapid=0;
+		semester=0;
+		name="";
+		cgpa=0.0;
+			
+	}
+	student(int sapid,string name,int smester,float cgpa)
+	{
+		this->sapid=sapid;
+		this->name=name;
+		this->semester=semester;
+		this->cgpa=cgpa;
+	}
+	student(int sapid,string name)
+	{
+		this->sapid=sapid;
+		this->name=name;
+	}
+
+	student(int sapid,string name,float gpa)
+	{
+		this->sapid=sapid;
+		this->name=name;
+		this->cgpa=cgpa;
+	}
+		//copy constructor
+	student (student &std)
+	{
+		sapid=std.sapid;
+		name=std.name;
+		semester=std.semester;
+		cgpa=std.cgpa;
+		
+	}
+	//destructor
+	~student(){
+		cout<<"destructor of student is running:";
+	}
+	
+};
+int main()
+{
+	student s1;
+	s1.sapid=46935;
+	s1.name="ibrahim";
+	s1.semester=2;
+	s1.cgpa=2.5;
+	cout<<"Details of Student 1 : "<<endl;
+	cout<<"sap id:"<<s1.sapid<<endl;
+	cout<<"name:"<<s1.name<<endl;
+	cout<<"semester"<<s1.semester<<endl;
+	cout<<"cgpa:"<<s1.cgpa<<endl;
+	return 0;	
+}
